8d6569614d8b82b47ecae69ecb0f1e6311393673
heizence <heizence6626@gmail.com>
2025-03-11 15:35:41 +0900

    add jwt-utils

---
+++ api/services/jwt-utils.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const generateJWT = (accountId, type, name) => {
+  return jwt.sign(
+    {
+      accountId,
+      type,
+      name,
+    },
+    process.env.JWT_SECRET, // Secret key stored in .env
+    { expiresIn: "365d" } // Token valid for 365 days
+  );
+};
+
+const verifyToken = token => {
+  if (!token) {
+    return res.status(401).send("Access Denied: No token provided");
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.JWT_SECRET); // Verify token using the secret key
+  } catch (err) {
+    res.status(400).send("Invalid Token");
+  }
+};
+
+module.exports = {
+  generateJWT,
+  verifyToken,
+};
