3431dda4951fd1df88a8b1b5f8ed113454f402bf
heizence <heizence6626@gmail.com>
2025-04-11 10:53:19 +0900

    add getDeploymentHistory API, multipart API form

--- cli/src/libs/APIs.js
+++ cli/src/libs/APIs.js
@@ -56,16 +56,69 @@ const commonAPI = async (method = "post", reqPath, reqParamsOrBody, needAuth = t
     });
 };
 
+const commonMultipartAPI = async (reqPath, formData, needAuth = true) => {
+  let accessToken;
+  if (needAuth) {
+    const homeDirectory = os.homedir();
+    const tokenDirectory = path.join(homeDirectory, ".greenlight-codepush.config"); // /Users/{username}/.code-push.config
+    const tokenFilePath = path.join(tokenDirectory, "token.json");
+
+    if (fs.existsSync(tokenFilePath)) {
+      const data = fs.readFileSync(tokenFilePath, "utf8");
+      const { token } = JSON.parse(data);
+      accessToken = token;
+    } else {
+      console.log("You need to login in from this machine.");
+      return;
+    }
+  }
+  const baseUrl = process.env.SERVER_URL || "http://localhost";
+  const port = process.env.PORT || 3000;
+
+  //console.log("baseURL : ", `${baseUrl}:${port}/`);
+  return axios({
+    baseURL: `${baseUrl}:${port}/`,
+    url: reqPath,
+    method: "post",
+    headers: {
+      Authorization: accessToken, // Send token in the request
+      "Content-Type": "multipart/form-data",
+      Accept: "application/json",
+    },
+    data: formData,
+    timeout: 40000,
+  })
+    .then(response => {
+      //console.log(`[commonMultipartAPI] [${reqPath}]success : `, response.data);
+      //console.log(response.data.message);
+
+      const dataToDisplay = response.data.data;
+      if (dataToDisplay) {
+        if (dataToDisplay.table) console.table(dataToDisplay.table);
+      }
+      return response.data;
+    })
+    .catch(error => {
+      //console.log(`[commonMultipartAPI] [${path}]error : `, error.response?.data);
+      console.log(chalk.red("[Error] " + error.response?.data.message));
+      return error.response?.data;
+    });
+};
+
 const verifyToken = token => commonAPI("post", "auth/verifyToken", { token }, false);
 const addApp = body => commonAPI("post", "app/add", body);
 const getApplist = () => commonAPI("get", "app/list", {});
+const getDeploymentHistory = params => commonAPI("get", "deployment/history", params);
 const removeApp = body => commonAPI("post", "app/remove", body);
 const renameApp = body => commonAPI("post", "app/rename", body);
+
+const uploadBundleFile = body => commonMultipartAPI("post", "codepush/upload", body);
 
 module.exports = {
   verifyToken,
   addApp,
   getApplist,
+  getDeploymentHistory,
   removeApp,
   renameApp,
 };
