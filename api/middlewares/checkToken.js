35f9eb171031609b3e04facc554087af6b1f4451
heizence <heizence6626@gmail.com>
2025-04-16 13:10:50 +0900

    add user idendity check to checkTokenmiddleware

--- api/middlewares/checkToken.js
+++ api/middlewares/checkToken.js
@@ -1,29 +1,38 @@
 const dotenv = require("dotenv");
 const { verifyToken } = require("../services/jwt-utils");
 const { responseDto } = require("../DTO/response");
+const { findUser } = require("../database/query");
 dotenv.config();
 
 // Token verification middleware
-const CheckTokenMiddleware = (req, res, next) => {
+const CheckTokenMiddleware = async (req, res, next) => {
   const token = req.headers["authorization"];
 
   if (!token) {
     console.log("no token!");
     responseDto;
-    return res.status(401).json(responseDto(false, 401, "No token provided"));
+    return res.status(401).json(responseDto(false, "No token provided"));
   }
 
   // Verify the token using your secret key
   const tokenInfo = verifyToken(token);
   //console.log("tokenInfo : ", tokenInfo);
   if (tokenInfo) {
-    console.log("[CheckTokenMiddleWare]token is valid!");
     req.tokenInfo = tokenInfo;
-    next();
-  } else {
-    console.log("[CheckTokenMiddleWare]token is invalid!");
-    return res.status(403).json(responseDto(false, 403, "Invalid token. Please login again."));
+
+    const accountId = tokenInfo.accountId;
+    const checkUser = await findUser(accountId);
+
+    if (checkUser.length > 0) {
+      console.log("[CheckTokenMiddleWare]token is valid!");
+      const userId = checkUser[0].id;
+      req.userId = userId;
+      next();
+      return;
+    }
   }
+  console.log("[CheckTokenMiddleWare]token is invalid!");
+  return res.status(403).json(responseDto(false, "Invalid token. Please login again."));
 };
 
 module.exports = CheckTokenMiddleware;
